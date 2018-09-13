1035c233a9816e6bb0af98d608fe15fd5503b110
Andy Zalit <andyzalit@gmail.com>
2018-09-13 14:37:13 -0700

    Challenge 1 2

--- part1/server.js
+++ part1/server.js
@@ -6,6 +6,19 @@ app.get('/yourroute', function(req, res) {
   res.send("stuff");
 });
 
+app.get('/hello', function(req, res) {
+  res.send("Hello");
+})
+
+app.post('/create/:name', function(req, res) {
+  var name = `Your name is: ${req.params.name}`;
+  res.send(name);
+})
+
+app.get('/', function(req, res) {
+  res.sendFile('/index.html'));
+})
+
 app.use(function(req, res) {
   res.sendStatus(404);
 });
